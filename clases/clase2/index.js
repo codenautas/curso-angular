9e83b60ca40658d225e334ea54cce31e693f18e4
Emilio <emilioplatzer@gmail.com>
2015-08-04 12:49:39 -0300

    ejemplo, usando ng-bind

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -8,18 +8,22 @@
         this.publicaciones=[{
             title:'Anuario estadístico',
             url:'http://estadisticaciudad.gob.ar/publicaciones/1.pdf',
+            type:'PDF',
             year:2015
         }, {
             title:'Cartografía',
             url:'http://estadisticaciudad.gob.ar/publicaciones/2.pdf',
+            type:'PDF',
             year:2014
         }, {
             title:'Encuesta Anual de Hogares',
-            url:'http://estadisticaciudad.gob.ar/publicaciones/3.pdf',
+            url:'http://estadisticaciudad.gob.ar/publicaciones/3.doc',
+            type:'DOC',
             year:2013
         }, {
             title:'Canasta alimentaria',
             url:'http://estadisticaciudad.gob.ar/publicaciones/5.pdf',
+            type:'PDF',
             year:2013
         }];
         // this.publicaciones=[];
