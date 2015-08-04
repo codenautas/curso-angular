cb5b486cd1658635b93d3b542138ffeb462ed1ed
Emilio <emilioplatzer@gmail.com>
2015-08-04 11:38:39 -0300

    ejemplo: cuando la lista esté vacía pongo un mensaje. uso ng-hide

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -6,10 +6,7 @@
     
     app.controller("ListaPub",function(){
         this.publicaciones=[2015, 2014, 2013];
+        // this.publicaciones=[];
     });
-
-    app.controller("lista2",["$scope", function($scope){
-        $scope.publicaciones=[1995, 1994, 1993, 1992];
-    }]);
 
 })();
