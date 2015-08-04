91a6a9685f19c09815c2e29285092fb5f2100a09
Emilio <emilioplatzer@gmail.com>
2015-08-04 11:04:23 -0300

    el ejemplo ya tiene ng-controller

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -4,6 +4,10 @@
 
     var app = angular.module("publicacionesApp",[]);
     
+    app.controller("ListaPub",function(){
+        this.publicaciones=[2015, 2014, 2013];
+    });
+    
     console.log(app);
 
 })();
