a014a70bb9424f740730742e37a9d891adc6c367
Emilio <emilioplatzer@gmail.com>
2015-08-04 13:03:29 -0300

    ejemplo: Agregamos una imagen en forma directa.

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -8,21 +8,25 @@
         this.publicaciones=[{
             title:'Anuario estadístico',
             url:'http://estadisticaciudad.gob.ar/publicaciones/1.pdf',
+            logo:'anuario.png',
             type:'PDF',
             year:2015
         }, {
             title:'Cartografía',
             url:'http://estadisticaciudad.gob.ar/publicaciones/2.pdf',
+            logo:'mapa.png',
             type:'PDF',
             year:2014
         }, {
             title:'Encuesta Anual de Hogares',
             url:'http://estadisticaciudad.gob.ar/publicaciones/3.doc',
+            logo:'libro.png',
             type:'DOC',
             year:2013
         }, {
             title:'Canasta alimentaria',
             url:'http://estadisticaciudad.gob.ar/publicaciones/5.pdf',
+            logo:'inexistente.png',
             type:'PDF',
             year:2013
         }];
