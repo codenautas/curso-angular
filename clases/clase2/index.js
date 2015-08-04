eeb097b8cdcae8e1ce6a14eeee29e190dc7c0631
Emilio <emilioplatzer@gmail.com>
2015-08-04 11:28:31 -0300

    Cuando se usa la forma $scope hay que tener cuidado por la mimificación
    hay que poner un array indicando los nombres de los parámetros.

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -8,8 +8,8 @@
         this.publicaciones=[2015, 2014, 2013];
     });
 
-    app.controller("lista2",function($scope){
+    app.controller("lista2",["$scope", function($scope){
         $scope.publicaciones=[1995, 1994, 1993, 1992];
-    });
+    }]);
 
 })();
