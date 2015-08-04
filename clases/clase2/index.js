d1020ea572218682bc7c310a18bdd5700c6e4e74
Emilio <emilioplatzer@gmail.com>
2015-08-04 12:46:33 -0300

    ejemplo: agregamos los PDF

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -7,15 +7,19 @@
     app.controller("ListaPub",function(){
         this.publicaciones=[{
             title:'Anuario estadístico',
+            url:'http://estadisticaciudad.gob.ar/publicaciones/1.pdf',
             year:2015
         }, {
             title:'Cartografía',
+            url:'http://estadisticaciudad.gob.ar/publicaciones/2.pdf',
             year:2014
         }, {
             title:'Encuesta Anual de Hogares',
+            url:'http://estadisticaciudad.gob.ar/publicaciones/3.pdf',
             year:2013
         }, {
             title:'Canasta alimentaria',
+            url:'http://estadisticaciudad.gob.ar/publicaciones/5.pdf',
             year:2013
         }];
         // this.publicaciones=[];
