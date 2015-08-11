1646131e5e6baccdcacbe999504ad55523ee81b3
Franco <francogriecoo@gmail.com>
2015-08-11 12:35:10 -0300

    href

---
+++ clases/clase3/js/app.js
@@ -0,0 +1,24 @@
+(function(){
+
+	var app= angular.module("encuestasApp", ["ngRoute"]);
+	
+	app.config(function ($routeProvider) {
+		$routeProvider.when("/encuestas",{
+			templateUrl: "views/lista.html"
+		}).when("/encuestas/:encId/forms/:formId", {
+			templateUrl: "views/form.html"
+		}).otherwise({
+			redirectTo: "/encuestas"
+		});
+	});
+
+	app.controller("listaController", function($http){
+		var vm = this;
+		vm.lista = [];
+		$http.get("data/eah2013.json").then(function (resp){
+			vm.lista = resp.data;		
+		});
+	
+	});
+	
+})();
