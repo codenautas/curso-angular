de36894ea80dcee992373094980aac7b11e211bf
Elba Tonini <ebtonini@gmail.com>
2015-08-11 12:27:34 -0300

    agregamos la funcionalidad de router

--- clases/clase3/js/app.js
+++ clases/clase3/js/app.js
@@ -1,5 +1,15 @@
 (function(){
-    var app = angular.module("encuestasApp", []);
+    var app = angular.module("encuestasApp", ["ngRoute"]);
+    //la fucionalidad del routing:
+    app.config(function($routeProvider){
+        $routeProvider.when("/encuestas", {
+            templateUrl:"views/lista.html"
+        }).when("/encuestas/:encId/forms/:formId", {
+            templateUrl: "views/form.html"
+        }).otherwise({
+            redirectTo: "/encuestas"
+        });
+    });
     
     app.controller("listaController", function ($http) {
         var vm = this;
