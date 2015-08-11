3d199545ee2e4dd5e3e777a243b784b791c31e22
Elba Tonini <ebtonini@gmail.com>
2015-08-11 11:34:29 -0300

    Empezamos a definir encuestasApp

---
+++ clases/clase3/js/app.js
@@ -0,0 +1,14 @@
+(function(){
+    var app = angular.module("encuestasApp", []);
+    
+    app.controller("listaController", function ($http) {
+        var vm = this;
+        vm.lista = [];
+        
+        $http.get("data/eah2013.json").then(function(resp){
+            vm.lista = resp.data
+        });
+    
+    });
+
+})()
