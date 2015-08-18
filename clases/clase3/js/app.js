d54b73b64ec0301b88c3b418e978be5445e0404b
Gabriel Montes <gabriel@iamat.com>
2015-08-17 22:39:12 -0300

    Avance clase 3

---
+++ clases/clase3/js/app.js
@@ -0,0 +1,33 @@
+(function () {
+
+    var app = angular.module("encuestasApp", ["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider.when("/encuestas", {
+            templateUrl: "views/lista.html"
+        }).when("/encuestas/:encId/forms/:formId", {
+            templateUrl: "views/form.html"
+        }).otherwise({
+            redirectTo: "/encuestas"
+        });
+    });
+
+    app.controller("listaController", function ($http) {
+        var vm = this;
+        vm.lista = [];
+        $http.get("data/eah2013.json").then(function (resp) {
+            vm.lista = resp.data;
+        });
+    });
+
+    app.controller("formController", function ($routeParams, $http) {
+        var vm = this;
+        var encId = $routeParams.encId.toLowerCase();
+        var formId = $routeParams.formId.toLowerCase();
+        $http.get("data/" + encId + "_" + formId + ".json").then(function (resp) {
+            vm.id = resp.data.id;
+            vm.nombre = resp.data.nombre;
+        });
+    });
+
+})();
