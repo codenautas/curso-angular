e1fe21df5b3ab80d3dbc73f73dd109e6cddf575e
estefi-capece <eycapece@gmail.com>
2015-08-18 12:59:01 -0300

    Agregamos un filtro sobre una de las preguntas

--- clases/clase4/js/app.js
+++ clases/clase4/js/app.js
@@ -35,7 +35,7 @@
         });
     });
 
-    app.controller("formController", function ($routeParams, $http) {
+    app.controller("formController", function ($routeParams, $http,$scope) {
         var vm = this;
 
         // precargar formulario vacío
@@ -49,7 +49,38 @@
             vm.id = resp.data.id;
             vm.nombre = resp.data.nombre;
             vm.preguntas = resp.data.preguntas;
+            var watches=[];
+            vm.preguntas.filter(function(pregunta){
+                return pregunta.filtro;
+            }).forEach(function(pregunta){
+                watches.push(pregunta.filtro);
+            });
+/*            $scope.$watchCollection(function(scope){
+                return watches.map(function(filtro){
+                    var split= filtro.campo.split(".");
+                    var valor=
+                });
+            })*/
         });
+        vm.grabar=function(){
+            var url="respuesta/" + encId + "/" + formId; 
+            $http.post(url,vm.respuestas).then(function(resp){
+                // Exito
+                alert("Envío exitoso");
+            },function(resp){
+                alert("Hubo problemas al enviar la respuesta");
+            });
+        };
+        vm.filtros={};
+        $scope.$watch(function(scope){
+            return vm.respuestas && vm.respuestas.DI1 && vm.respuestas.DI1.edad; 
+        },function(){
+            if ( vm.respuestas && vm.respuestas.DI1 &&vm.respuestas.DI1.edad >=10){
+                vm.filtros.T1 = false;
+            }else{
+                vm.filtros.T1=true;
+            }
+        })
     });
 
 })();
